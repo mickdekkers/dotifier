0dab84dae5d117b54fc2e7418c91c26a2d5620c0
Soullesswaffle <mickeydekkers@gmail.com>
2015-10-22 02:24:43 +0200

    Add tests for binary, octal and hexadecimal literals

--- test.js
+++ test.js
@@ -24,32 +24,69 @@ describe('dotifier', function () {
     });
   });
   describe('.encode()', function () {
-    it('should return example@gmail.com when n = 0', function () {
-      expect(dotifier.encode('example@gmail.com', 0)).to.equal('example@gmail.com');
+    describe('decimal', function () {
+      it('should return example@gmail.com when n = 0', function () {
+        expect(dotifier.encode('example@gmail.com', 0)).to.equal('example@gmail.com');
+      });
+      it('should return exampl.e@gmail.com when n = 1', function () {
+        expect(dotifier.encode('example@gmail.com', 1)).to.equal('exampl.e@gmail.com');
+      });
+      it('should return e.x.a.m.p.l.e@gmail.com when n = 63', function () {
+        expect(dotifier.encode('example@gmail.com', 63)).to.equal('e.x.a.m.p.l.e@gmail.com');
+      });
     });
-    it('should return exampl.e@gmail.com when n = 1', function () {
-      expect(dotifier.encode('example@gmail.com', 1)).to.equal('exampl.e@gmail.com');
+    describe('binary', function () {
+      it('should return example@gmail.com when n = 0b0', function () {
+        expect(dotifier.encode('example@gmail.com', 0b0)).to.equal('example@gmail.com');
+      });
+      it('should return exampl.e@gmail.com when n = 0b1', function () {
+        expect(dotifier.encode('example@gmail.com', 0b1)).to.equal('exampl.e@gmail.com');
+      });
+      it('should return e.x.a.m.p.l.e@gmail.com when n = 0b111111', function () {
+        expect(dotifier.encode('example@gmail.com', 0b111111)).to.equal('e.x.a.m.p.l.e@gmail.com');
+      });
     });
-    it('should return e.x.a.m.p.l.e@gmail.com when n = 63', function () {
-      expect(dotifier.encode('example@gmail.com', 63)).to.equal('e.x.a.m.p.l.e@gmail.com');
+    describe('octal', function () {
+      it('should return example@gmail.com when n = 0o0', function () {
+        expect(dotifier.encode('example@gmail.com', 0o0)).to.equal('example@gmail.com');
+      });
+      it('should return exampl.e@gmail.com when n = 0o1', function () {
+        expect(dotifier.encode('example@gmail.com', 0o1)).to.equal('exampl.e@gmail.com');
+      });
+      it('should return e.x.a.m.p.l.e@gmail.com when n = 0o77', function () {
+        expect(dotifier.encode('example@gmail.com', 0o77)).to.equal('e.x.a.m.p.l.e@gmail.com');
+      });
     });
-    it('should return null when n > capacity', function () {
-      expect(dotifier.encode('example@gmail.com', 64)).to.be.null;
+    describe('hexadecimal', function () {
+      it('should return example@gmail.com when n = 0x0', function () {
+        expect(dotifier.encode('example@gmail.com', 0)).to.equal('example@gmail.com');
+      });
+      it('should return exampl.e@gmail.com when n = 0x1', function () {
+        expect(dotifier.encode('example@gmail.com', 0x1)).to.equal('exampl.e@gmail.com');
+      });
+      it('should return e.x.a.m.p.l.e@gmail.com when n = 0x3f', function () {
+        expect(dotifier.encode('example@gmail.com', 0x3f)).to.equal('e.x.a.m.p.l.e@gmail.com');
+      });
     });
-    it('should return null when n < 0', function () {
-      expect(dotifier.encode('example@gmail.com', -1)).to.be.null;
-    });
-    it('should return null when email is invalid', function () {
-      expect(dotifier.encode('examplegmail.com', 0)).to.be.null;
-      expect(dotifier.encode('@l.com', 0)).to.be.null;
-      expect(dotifier.encode('e@', 0)).to.be.null;
-      expect(dotifier.encode('@', 0)).to.be.null;
-    });
-    it('should return null when n is not an integer', function () {
-      expect(dotifier.encode('example@gmail.com', undefined)).to.be.null;
-      expect(dotifier.encode('example@gmail.com', null)).to.be.null;
-      expect(dotifier.encode('example@gmail.com', 'Hello, world!')).to.be.null;
-      expect(dotifier.encode('example@gmail.com', 1.23456789)).to.be.null;
+    describe('invalid input', function () {
+      it('should return null when n > capacity', function () {
+        expect(dotifier.encode('example@gmail.com', 64)).to.be.null;
+      });
+      it('should return null when n < 0', function () {
+        expect(dotifier.encode('example@gmail.com', -1)).to.be.null;
+      });
+      it('should return null when email is invalid', function () {
+        expect(dotifier.encode('examplegmail.com', 0)).to.be.null;
+        expect(dotifier.encode('@l.com', 0)).to.be.null;
+        expect(dotifier.encode('e@', 0)).to.be.null;
+        expect(dotifier.encode('@', 0)).to.be.null;
+      });
+      it('should return null when n is not an integer', function () {
+        expect(dotifier.encode('example@gmail.com', undefined)).to.be.null;
+        expect(dotifier.encode('example@gmail.com', null)).to.be.null;
+        expect(dotifier.encode('example@gmail.com', 'Hello, world!')).to.be.null;
+        expect(dotifier.encode('example@gmail.com', 1.23456789)).to.be.null;
+      });
     });
   });
 });
